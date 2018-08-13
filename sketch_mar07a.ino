ecfbd647d57dd1ecb1a576726f4c07217f29834f
Piyush757 <32321201+Piyush757@users.noreply.github.com>
2018-08-13 18:53:41 +0530

    Add files via upload

---
+++ sketch_mar07a.ino
@@ -0,0 +1,32 @@
+int redLed = 12;
+int greenLed = 11;
+int buzzer = 10;
+int smokeA0 = A5;
+int sensorThres = 100;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redLed,OUTPUT);
+  pinMode(greenLed,OUTPUT);
+  pinMode(buzzer,OUTPUT);
+  pinMode(smokeA0,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int analogSensor = analogRead(smokeA0);
+  Serial.print("Pin A0:");
+  Serial.println(analogSensor);
+  if(analogSensor > sensorThres)
+  {
+    digitalWrite(redLed,HIGH);
+    digitalWrite(greenLed,LOW);
+    tone(buzzer,1000,200);
+  }
+  else
+  {
+    digitalWrite(redLed,LOW);
+    digitalWrite(greenLed,HIGH);
+  }
+  delay(100);
+}

0e4e0213fd4282361eee5d10ffd20aebe8486c8a
hacksaurav <32787860+hacksaurav@users.noreply.github.com>
2018-10-10 11:02:32 +0530

    Update sketch_mar07a.ino

--- sketch_mar07a.ino
+++ sketch_mar07a.ino
@@ -1,7 +1,7 @@
 int redLed = 12;
 int greenLed = 11;
 int buzzer = 10;
-int smokeA0 = A5;
+int smokeA0 = A5;//sensor is attached to the A5 analog pin of arduino
 int sensorThres = 100;
 void setup() {
   // put your setup code here, to run once:
@@ -15,7 +15,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int analogSensor = analogRead(smokeA0);
-  Serial.print("Pin A0:");
+  Serial.print("Pin A5:");
   Serial.println(analogSensor);
   if(analogSensor > sensorThres)
   {
